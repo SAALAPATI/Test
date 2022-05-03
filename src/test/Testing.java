488968c080bc9e30a68d9cce5bae53d72e5468b1
saalapati <saalapati@192.168.1.4>
2022-05-03 20:47:12 +0530

    this is commit 3

--- src/test/Testing.java
+++ src/test/Testing.java
@@ -5,7 +5,7 @@ public class Testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("testing 1");
-System.out.println("testing 2");
+System.out.println("testing some changes in this file");
 System.out.println("testman");
 System.out.println("postman");
 
