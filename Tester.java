fd7e6628980d4617d8f91eb95fd18df1a923e08e
Sai Neeraj Alapati <saineeraj.alapati@copart.com>
2022-05-03 19:40:23 +0530

    Create Tester.java

---
+++ Tester.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("testing 3");
+System.out.println("testing 4");
+
+	}
+
+}
